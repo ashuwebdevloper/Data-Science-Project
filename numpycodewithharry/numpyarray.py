622e567ed268af9846269f74384fcc375e9b10e2
ashuwebdevloper <aboycalledashish@gmail.com>
2026-03-22 21:54:24 +0530

    4

---
+++ numpycodewithharry/numpyarray.py
@@ -0,0 +1,10 @@
+import numpy as np
+# Creating a 1D NumPy array
+arr1 = np.array([1, 2, 3, 4, 5])
+print(arr1)
+# Creating a 2D NumPy array
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr2)
+# Checking type and shape
+print("Type:", type(arr1))
+print("Shape:", arr2.shape)
